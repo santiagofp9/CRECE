98be84340feec546f379092cab45ff82347c8e2d
santiagofp9 <santiagofp9@gmail.com>
2020-01-23 15:49:19 +0100

    problema
    No funciona el cambio de ciudad en el index verde

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
             <form class="form-inline" action="insert.php" method="post" id="ciuBus">
 				<label>Ciudad </label>
 				<select name="ciudad" id="ciudad" class="form-control">
+					<option value="3117735">Madrid</option>
 					<?php 
 						include 'conex.php';
 						$consulta = "SELECT id, ciudad FROM ciudades";
@@ -222,10 +223,9 @@
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="app.js"></script>
  <script text="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.js"></script>
 <script text="text/javascript" src="jquery-3.4.1.js"></script>
-<script text="text/javascript" src="conex.js"></script>
+<!--<script text="text/javascript" src="conex.js"></script>-->
 <script text="text/javascript" src="weather.js"></script>
 
 
--- weather.js
+++ weather.js
@@ -1,30 +1,37 @@
  //PRONÓSTICO ACTUAL
  $(document).ready(function(){
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?id=3117735&lang=sp&units=metric&APPID=65a9b2caaaba5d1b1762d5889a2637ce",
-        datatype : "json",
-        type: "GET",
+	 
+	
+		 $("#ciudad").change(function(){
+		console.log(1);
+		$("#ciudad option:selected").each(function(){
+			var city = $(this).val();
+		})
+			 
+		$.ajax({
+			url: "http://api.openweathermap.org/data/2.5/weather?id="+city+"&lang=sp&units=metric&APPID=65a9b2caaaba5d1b1762d5889a2637ce",
+			datatype : "json",
+			type: "GET",
 
-        success: function (respuesta) {
-          console.log(respuesta);
-          $("#climact").html(`<img src="icons/${respuesta.weather[0].icon}.png"/>`);
-          $('#desact').html(respuesta.weather[0].description);
-          $("#tempact").html(Math.floor(respuesta.main.temp) + "<br>"); 
-          $("#tempmin").html(respuesta.main.temp_min + "<br>");
-          $("#tempmax").html(respuesta.main.temp_max + "<br>");
-          $("#speedwindy").html(respuesta.wind.speed + "<br>");
-          $("#cloud").html(respuesta.clouds.all+ "%" + "<br>");
-          $("volrain").html(respuesta.rain1h + "<br>");
-          $("volsnow").html(respuesta.snow1h + "<br>");
-                  
-        },
-        error: function (jqXHR, textStatus, errorThrown){
-          console.log("ERROR :  " + errorThrown);
-        }
+			success: function (respuesta) {
+			  console.log(respuesta);
+			  $("#climact").html(`<img src="icons/${respuesta.weather[0].icon}.png"/>`);
+			  $('#desact').html(respuesta.weather[0].description);
+			  $("#tempact").html(Math.floor(respuesta.main.temp) + "<br>"); 
+			  $("#tempmin").html(respuesta.main.temp_min + "<br>");
+			  $("#tempmax").html(respuesta.main.temp_max + "<br>");
+			  $("#speedwindy").html(respuesta.wind.speed + "<br>");
+			  $("#cloud").html(respuesta.clouds.all+ "%" + "<br>");
+			  $("volrain").html(respuesta.rain1h + "<br>");
+			  $("volsnow").html(respuesta.snow1h + "<br>");
 
-        
+			},
+			error: function (jqXHR, textStatus, errorThrown){
+			  console.log("ERROR :  " + errorThrown);
+			}  
       });
-
+		 });
+		 console.log(city);
   });
 
 
