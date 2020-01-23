c0d4f6ec93cd51c0e6c147628c90f7736565107f
MagdalenaRocio <57403616+MagdalenaRocio@users.noreply.github.com>
2020-01-23 17:48:14 +0100

    Add files via upload

--- registrar.php
+++ registrar.php
@@ -8,36 +8,26 @@
     
 </head>
 <body>
-   
-    <form method="POST"action="insertar.php">
-    
-    <input type="email" name="email" id="email"placeholder="insert your email"required onblur="validarEmail()">
-    <div id="mensajecorreo"></div>
-    <input type="password"name="password"id="password"placeholder="insert your password"required onblur="validarPassword()">
-    <div id="mensajepassword">
-    <input type="password"name="confirm password"id="confirm password" placeholder="confirm password" required onblur="validarPassword()">
-    <div id="mensajepassword">
-    <input type="submit" name="enviar">
 
-    
-    <?php include "conexion.php";
-    $consulta="select EMAIL from login";
-    $r= mysqli_query($link,$consulta);
-    while ($arr = mysqli_fetch_array($r)){ ?>
-        <option value="<?php echo $arr[0]?>">
-        
-        </option>
-       
-   <?php } ?>
-    </form>
+<form method="POST"action="insertar.php">
+<input type="email" name="email" id="email"placeholder="insert your email"required onblur="validarEmail()">
+<div id="mensajecorreo"></div>
+<input type="password"name="password"id="password"placeholder="insert your password"required onblur="validarPassword()">
+<div id="mensajepassword"></div>
+<input type="password"name="confirm password"id="confirm password" placeholder="confirm password" required onblur="validarPassword()">
+<div id="mensajepassword2"></div>
+<input type="submit" name="enviar">
+
+<?php include "conexion.php";
+$consulta="select EMAIL from login";
+$r= mysqli_query($link,$consulta);
+while ($arr = mysqli_fetch_array($r)){ ?>
+<option value="<?php echo $arr[0]?>">
+</option>
+
+<?php } ?>
+</form>
 <script src="validar.js"></script>
 
 </body>
 </html>
-
-
-
-
-
-
-    
